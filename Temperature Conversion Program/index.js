8eb9bfc68041cb6c128d63266f9281b8860625ef
Vidushanka Lakshan <154110983+vidushankalj@users.noreply.github.com>
2024-04-30 16:28:38 +0530

    Temperature Conversion Program

---
+++ Temperature Conversion Program/index.js
@@ -0,0 +1,26 @@
+// TEMPERATURE CONVERSION PROGRAM
+
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+let temp;
+
+
+function convert(){
+
+    temp = Number(textBox.value);
+
+    if(toFahrenheit.checked){
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + "°F"
+    }
+    else if(toCelsius.checked){
+        temp = (temp - 32) * (5/9);
+        result.textContent = temp.toFixed(1) + "°C"
+    }
+    else{
+        result.textContent = "Select a unit";
+    }
+
+}
